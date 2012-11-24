00b73584a236f5d542d9b96fa9e1118d9654cd65
Charles Marshall <marshac3@aston.ac.uk>
2012-11-24 15:42:29 +0000

    added fileReader, Dictionary, DictionaryEntry classes

--- src/DictionaryProgram.java
+++ src/DictionaryProgram.java
@@ -1,4 +1,13 @@
 
 public class DictionaryProgram {
 
+	private static DictionaryProgram instance;
+	
+	public void main(String[] args){
+		instance = new DictionaryProgram();
+	}
+	
+	private DictionaryProgram(){
+		System.out.println("Welcome to the Chinese to English dictionary application!");
+	}
 }
