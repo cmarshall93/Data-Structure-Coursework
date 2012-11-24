e727992ee8b61d2fbc5a2a9bb7bb1978eee7b02c
Charles Marshall <marshac3@aston.ac.uk>
2012-11-24 16:23:47 +0000

    added class files

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -1,4 +1,33 @@
+import java.util.HashMap;
+
 
 public class Dictionary {
 
+	private HashMap<String, DictionaryEntry> tradChineseMap;
+	private HashMap<String, DictionaryEntry> simpChineseMap;
+	private HashMap<String, DictionaryEntry> pinyinMap;
+	private HashMap<String, DictionaryEntry> englishMap;
+	
+	public Dictionary(){
+		tradChineseMap = new HashMap<String, DictionaryEntry>();
+		simpChineseMap = new HashMap<String, DictionaryEntry>();
+		pinyinMap = new HashMap<String, DictionaryEntry>();
+		englishMap = new HashMap<String, DictionaryEntry>();
+	}
+	
+	public void addTradChinese(String key, DictionaryEntry entry){
+		tradChineseMap.put(key, entry);
+	}
+	
+	public void addSimpleChinese(String key, DictionaryEntry entry){
+		simpChineseMap.put(key, entry);
+	}
+	
+	public void addPinyin(String key, DictionaryEntry entry){	
+		pinyinMap.put(key, entry);
+	}
+	
+	public void addEnglish(String key, DictionaryEntry entry){
+		englishMap.put(key, entry);
+	}
 }
--- src/DictionaryEntry.java
+++ src/DictionaryEntry.java
@@ -1,4 +1,32 @@
 
 public class DictionaryEntry {
 
+	private String tradChinese;
+	private String simpleChinese;
+	private String pinyin;
+	private String english;
+	
+	public DictionaryEntry(String[] array){
+		tradChinese = array[0];
+		simpleChinese = array[1];
+		pinyin = array[2];
+		english = array[3];
+	}
+	
+	public String getTradChinese(){
+		return tradChinese;
+	}
+	
+	public String getSimpleChinese(){
+		return simpleChinese;
+	}
+	
+	public String getPinYin(){
+		return pinyin;
+	}
+	
+	public String getEnglish(){
+		return english;
+	}
+	
 }
